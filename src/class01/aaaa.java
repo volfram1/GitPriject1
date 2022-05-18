1a25410921d128251c87a7536fe86c231f40f78f
volfram1 <atomczak256@gmail.com>
2022-05-17 23:20:58 -0400

    upadated commit

--- src/class01/aaaa.java
+++ src/class01/aaaa.java
@@ -1,13 +1,15 @@
 package class01;
 
 public class aaaa {
-
+// team lead project
     public static void main(String[] args) {
         System.out.println("I am Groot");
         System.out.println("I am Groot2");
         System.out.println("I am Groot3");
 
         System.out.println("I ma correct code");
-        System.out.println("I am not confused"); 
+        System.out.println("I am not confused");
+
+        System.out.println("hello my friend");
     }
 }
