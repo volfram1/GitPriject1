7b52a887f1344baae936768a94964570e126c687
volfram1 <atomczak256@gmail.com>
2022-05-17 21:02:12 -0400

    2nd commit

---
+++ src/class01/aaaa.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class aaaa {
+
+    public static void main(String[] args) {
+        System.out.println("I am Groot");
+        System.out.println("I am Groot");
+        System.out.println("I am Groot");
+    }
+}
